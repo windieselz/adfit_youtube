55f094685fb71643f0f6914fd55f2eaaa7376fd0
unknown <shortcutz_s@hotmail.com>
2014-05-20 19:09:19 +0700

    fix

--- src/Android/com/adfit/plugins/AdfitYoutube.java
+++ src/Android/com/adfit/plugins/AdfitYoutube.java
@@ -10,6 +10,7 @@ package com.adfit.plugins;
  * 
  */
 
+import java.io.Serializable;
 import java.util.LinkedHashMap;
 
 import org.apache.cordova.CallbackContext;
@@ -18,6 +19,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.Context;
 import android.net.Uri;
@@ -26,11 +28,28 @@ import android.util.Log;
 
 import com.adfit.plugins.PlayerViewDemoActivity;
 
-public class AdfitYoutube extends CordovaPlugin {
+public class AdfitYoutube extends CordovaPlugin implements Serializable {
+	
+	private CallbackContext callback = null;
+	
+	private static AdfitYoutube _instance;
 
+    public AdfitYoutube(){
+    	
+    }
+
+    public static AdfitYoutube getInstance(){
+        if (_instance == null)
+        {
+            _instance = new AdfitYoutube();
+        }
+        return _instance;
+    }
+	
 	@Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		Log.d("Youtube","start android cordova execute");
+		_instance = this;
 		if(action.equals("openYoutube")) {
 			String ytStr = args.getString(0);
 			JSONObject jsonObj = new JSONObject(ytStr);
@@ -43,21 +62,50 @@ public class AdfitYoutube extends CordovaPlugin {
 		return false;
 	}
 	
-	private void open(String vid, CallbackContext callbackContext) {
-			
-		//Intent intent = new Intent(null, Uri.parse("ytv://"+vid), this.cordova.getActivity(), OpenYouTubePlayerActivity.class);
+	private void open(String vid, CallbackContext callbackContext) {			
 		PlayerViewDemoActivity ytView = new PlayerViewDemoActivity();
 		Context context = this.cordova.getActivity().getApplicationContext();
 		Intent myIntent = new Intent(context, PlayerViewDemoActivity.class);
 		myIntent.putExtra("youtubeid", vid);
-		/*
-		LinkedHashMap<String, Object> obj = new LinkedHashMap<String, Object>();
-        obj.put("callback", callbackContext); 
-		Bundle b = new Bundle();
-        b.putSerializable("bundleobj", obj);
-        myIntent.putExtras(b);
-        */
-		this.cordova.getActivity().startActivity(myIntent);
+		this.callback = callbackContext;
+		//this.cordova.getActivity().startActivityForResult(myIntent,Activity.RESULT_OK);
+		this.cordova.startActivityForResult((CordovaPlugin) this, myIntent, 0);
+	}
+	
+	public final void setResult (int resultCode){
+		if(resultCode == 1){
+			Log.d("Youtube","watch ended");
+		}else{
+			Log.d("Youtube","watch failed");
+		}
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Log.d("Youtube","Result code : "+resultCode);
+	}
+	
+	public void sendCallback(boolean status){
+		if(this.callback == null){
+			Log.d("Youtube","callback is null");
+			return ;
+		}
+		
+		if (status) { 
+			 cordova.getActivity().runOnUiThread(new Runnable() {
+		            public void run() {
+		            	callback.success("ok");
+		            }
+		        });
+			//this.callback.success("ok");
+        } else {
+        	//this.callback.error("Expected one non-empty string argument.");
+        	cordova.getActivity().runOnUiThread(new Runnable() {
+	            public void run() {
+	            	callback.error("failed");
+	            }
+	        });
+        }
+
 	}
 	
 }
